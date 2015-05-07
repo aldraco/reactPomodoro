b311f431911a8f7df882e79519f3bc5c2a52d0f5
A. Drake <a.l.drake713@gmail.com>
2015-05-07 11:58:11 -0400

    Refactoring state vars TODO modify start fxn to handle variable modes

--- scripts/App.js
+++ scripts/App.js
@@ -3,23 +3,34 @@ var React = require('react');
 var Pomodoro = React.createClass({
   getInitialState: function() {
     return {
-      seconds: 25*60,
-      minutes: 25,
       mode: 'work',     // opts: 'work', 'rest'
-      workInterval: 25*60,
+      interval : {
+        work: 25*60,
+        rest: 5*60
+      },
       restInterval: 5*60,
       defaultWorkTime: 25*60,
       defaultRestTime: 5*60
     };
   },
 
+  componentDidMount: function() {
+    console.log('hello world');
+  },
+
   addTime: function(e) {
     var selectedTimer = e.target.value;
     // will be workInterval or restInterval
     if (selectedTimer === 'rest') {
-      this.setState({restInterval: restInterval + 60});
+      this.setState({interval: {
+        rest: this.state.interval.rest + 60,
+        work: this.state.interval.work}
+      });
     } else {
-      this.setState({workInterval: workInterval + 60});
+      this.setState({interval: {
+        work: this.state.interval.work + 60,
+        rest: this.state.interval.rest}
+      });
     }  // adds a minute
   },
 
@@ -27,21 +38,26 @@ var Pomodoro = React.createClass({
     var selectedTimer = e.target.value;
     // will be workInterval or restInterval
     if (selectedTimer === 'rest') {
-      this.setState({restInterval: restInterval - 60});
+      this.setState({interval: {
+        rest: this.state.interval.rest - 60,
+        work: this.state.interval.work}
+      });
     } else {
-      this.setState({workInterval: workInterval - 60});
+      this.setState({interval: {
+        work: this.state.interval.work - 60,
+        rest: this.state.interval.rest}
+      });
     }  // adds a minute
   },
 
   handleStart: function(e) {
-    console.log("event is",e);
-    var selectedMode = e.target.value;
+
     this.timer = setInterval(function() {
       this.setState({seconds: this.state.seconds - 1});
       if (this.state.seconds <= 0) {
         // when the countdown stops, clear the timer
         clearInterval(this.timer);
-        // switch modes
+        // ... and switch modes
         this.switchModes();
         this.handleStart();
       }
@@ -50,21 +66,29 @@ var Pomodoro = React.createClass({
 
   switchModes: function() {
     // there is a toggle key available to switch modes
-    if (this.mode === 'work') {
-      this.setState({mode: 'rest', seconds: this.restInterval});
+
+    // first pause the current timer
+
+
+    // change the state
+    if (this.state.mode === 'work') {
+      this.setState({mode: 'rest', seconds: this.state.restInterval});
 
     } else {
-      this.setState({mode: 'work', seconds: this.workInterval});
+      this.setState({mode: 'work', seconds: this.state.workInterval});
     }
+
+    // then start a new timer on the new mode
   },
   handlePause: function() {
+    console.log("should pause");
     clearInterval(this.timer);
   },
 
   handleReset: function() {
     this.handlePause();
     // resetting brings you back to work mode
-    this.setState({seconds: this.state.defaultWorkTime, mode: 'work'});
+    this.setState({interval: {work: 25*60, rest: 5*60}, mode: 'work'});
   },
 
 
@@ -78,7 +102,15 @@ var Pomodoro = React.createClass({
         <button onClick={this.handleStart}>Start</button>
         <button onClick={this.handlePause}>Stop</button>
         <button onClick={this.handleReset}>Reset</button>
-        <h1>{this.state.seconds}</h1>
+        <button onClick={this.switchModes}>Toggle Mode</button>
+        <h2>work</h2>
+        <button onClick={this.addTime} value="work"> + </button>
+        <button onClick={this.subtractTime} value="work"> - </button>
+        <h2>rest</h2>
+        <button onClick={this.addTime} value="rest"> + </button>
+        <button onClick={this.subtractTime} value="rest"> - </button>
+        <h1>Work {this.state.interval.work}</h1>
+        <h1>Rest {this.state.interval.rest}</h1>
 
       </div>
     );
